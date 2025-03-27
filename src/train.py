b7d223cf1491f8fc8ac3fb850a83d67e6ada3bcc
Kabachok <alexeych102@gmail.com>
2025-03-27 22:44:45 +0300

    add tqdm for better representation

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 from typing import Callable
+from tqdm import tqdm
 
 import torch
 from torch.utils.data import DataLoader
@@ -9,10 +10,17 @@ def train_loop(
         loss_fn: Callable,
         optimizer: torch.optim,
         device: torch.device,
-        train_loader: DataLoader
+        train_loader: DataLoader,
+        epoch_num: int
 ):
     model.train()
-    for batch_idx, (images, labels) in enumerate(train_loader):
+    progress_bar = tqdm(
+        enumerate(train_loader),
+        total=len(train_loader),
+        desc=f"Epoch {epoch_num + 1}",
+        leave=False
+    )
+    for batch_idx, (images, labels) in progress_bar:
         images, labels = images.to(device), labels.to(device)
 
         prediction = model(images)
@@ -22,9 +30,9 @@ def train_loop(
         loss.backward()
         optimizer.step()
 
-        if batch_idx % 10 == 0:
-            print(f"loss: {loss.item():>7f}  [{batch_idx * len(images):>5d}/{len(train_loader.dataset):>5d}]")
-
+        progress_bar.set_postfix({
+            "Loss": f"{loss.item():.4f}"
+        })
 
 def test_model(
         model: torch.nn.Module,
@@ -37,16 +45,27 @@ def test_model(
     correct = 0
     count_processed_images = 0
 
+    progress_bar = tqdm(
+        test_dataloader,
+        desc="Testing",
+        leave=False
+    )
+
     with torch.no_grad():
-        for (images, labels) in test_dataloader:
+        for (images, labels) in progress_bar:
             images, labels = images.to(device), labels.to(device)
 
             prediction = model(images)
             test_loss += loss_fn(prediction, labels).item() # can be output for check loss
             correct += (prediction.round() == labels).type(torch.float).sum().item()
             count_processed_images += len(images)
+
+            progress_bar.set_postfix({
+                "Current Accuracy": f"{(correct / count_processed_images) * 100:.2f}%"
+            })
+
     accuracy = correct / count_processed_images
-    print(f"Test Accuracy: {(100 * accuracy):.2f}%")
+    print(f"\nTest Accuracy: {(100 * accuracy):.2f}%")
     return accuracy
 
 
@@ -61,15 +80,21 @@ def train_model(
 ):
     print("Using device:", device)
 
+    epoch_progress = tqdm(
+        range(epochs),
+        desc="Total Progress"
+    )
+
     # train model
-    for epoch in range(epochs):
+    for epoch in epoch_progress:
         print(f"Epoch {epoch + 1}")
         train_loop(
             model=model,
             loss_fn=loss_fn,
             optimizer=optimizer,
             device=device,
-            train_loader=train_dataloader
+            train_loader=train_dataloader,
+            epoch_num=epoch
         )
         test_model(
             model=model,
